be23513eeb3983a8f5b7a8b0453b2540fba22ebe
fyzn <46656725+fyzn@users.noreply.github.com>
2020-09-04 21:32:30 -0300

    Verificação funcionando

---
+++ JavaScript/Praticando/loteria/script.js
@@ -0,0 +1,46 @@
+var numeros = '';
+var acertados = [];
+var errados = [];
+
+$(document).ready(function() {
+    $("#btnVerificar").click(function() {
+        if (numeros.length) {
+            acertados = []
+            errados = []
+            numeros = []
+        }
+        var concurso = $("#selConcurso").val();
+        var numConcurso = $("#numConcurso").val();
+        var token = "5LIZAjgs0EARozO"
+
+        numeros = $("#txtNumeros").val();
+        numeros = numeros.split(' ');
+
+        var url = "https://apiloterias.com.br/app/resultado?loteria=" + concurso + "&token=" + token + "&concurso=" + numConcurso;
+
+        $.ajax({
+            url,
+            type: "get",
+            dataType: "json",
+            success: function(data) {
+                var numResultado = data.dezenas
+
+                for (var i = 0; i < numeros.length; i++) {
+                    if (numResultado.indexOf(numeros[i]) > -1) {
+                        acertados.push(numeros[i]);
+                    } else {
+                        errados.push(numeros[i]);
+                    }
+                }
+                $("#numAcertados").text(acertados);
+                $("#numErrados").text(errados);
+                $("#numAcertadosTotal").text(acertados.length)
+            },
+            error: function(error) {
+                console.log('Não entrou ' + error)
+            }
+        })
+    })
+})
+
+// 03 06 10 17 34 37
