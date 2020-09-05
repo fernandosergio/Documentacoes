093e797a13b2cd126c766be760db0f182ca45dca
fyzn <46656725+fyzn@users.noreply.github.com>
2020-09-05 12:52:06 -0300

    alert adicionado

--- JavaScript/Praticando/loteria/javascript/busca_api.js
+++ JavaScript/Praticando/loteria/javascript/busca_api.js
@@ -15,35 +15,40 @@ $(document).ready(function() {
 
         var inputs = document.querySelectorAll('input[type="checkbox"]:checked')
 
-        for (let i = 0; i < inputs.length; i++) {
-            numeros[i] = inputs[i].value
-        }
+        if (inputs.length) {
 
-        var url = "https://apiloterias.com.br/app/resultado?loteria=" + concurso + "&token=" + token + "&concurso=" + numConcurso;
-
-        $.ajax({
-            url,
-            type: "get",
-            dataType: "json",
-            success: function(data) {
-                var numResultado = data.dezenas
-
-                for (var i = 0; i < numeros.length; i++) {
-                    if (numResultado.indexOf(numeros[i]) > -1) {
-                        acertados.push(numeros[i]);
-                    } else {
-                        errados.push(numeros[i]);
-                    }
-                };
-                $('input[type="checkbox"]:checked').prop('checked', false);
-                $("#numAcertados").text(acertados);
-                $("#numErrados").text(errados);
-                $("#numAcertadosTotal").text(acertados.length);
-            },
-            error: function(error) {
-                console.log('Não entrou ' + error)
+            for (let i = 0; i < inputs.length; i++) {
+                numeros[i] = inputs[i].value
             }
-        })
+
+            var url = "https://apiloterias.com.br/app/resultado?loteria=" + concurso + "&token=" + token + "&concurso=" + numConcurso;
+
+            $.ajax({
+                url,
+                type: "get",
+                dataType: "json",
+                success: function(data) {
+                    var numResultado = data.dezenas
+
+                    for (var i = 0; i < numeros.length; i++) {
+                        if (numResultado.indexOf(numeros[i]) > -1) {
+                            acertados.push(numeros[i]);
+                        } else {
+                            errados.push(numeros[i]);
+                        }
+                    };
+                    $('input[type="checkbox"]:checked').prop('checked', false);
+                    $("#numAcertados").text(acertados);
+                    $("#numErrados").text(errados);
+                    $("#numAcertadosTotal").text(acertados.length);
+                },
+                error: function(error) {
+                    console.log('Não entrou ' + error)
+                }
+            })
+        } else {
+            alert('Selecione algum número!')
+        }
     })
 })
 
