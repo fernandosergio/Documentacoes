7c7826ac4a477aa652403af1055d7cacd9fbed2e
fyzn <46656725+fyzn@users.noreply.github.com>
2020-09-05 19:19:54 -0300

    script

--- JavaScript/Praticando/loteria/javascript/busca_api.js
+++ JavaScript/Praticando/loteria/javascript/busca_api.js
@@ -15,41 +15,85 @@ $(document).ready(function() {
 
         var inputs = document.querySelectorAll('input[type="checkbox"]:checked')
 
-        if (inputs.length) {
+        if (diadesorte || lotofacil || lotomania || megasena || quina) {
 
-            for (let i = 0; i < inputs.length; i++) {
-                numeros[i] = inputs[i].value
-            }
+            if (inputs.length) {
 
-            var url = "https://apiloterias.com.br/app/resultado?loteria=" + concurso + "&token=" + token + "&concurso=" + numConcurso;
-
-            $.ajax({
-                url,
-                type: "get",
-                dataType: "json",
-                success: function(data) {
-                    var numResultado = data.dezenas
-
-                    for (var i = 0; i < numeros.length; i++) {
-                        if (numResultado.indexOf(numeros[i]) > -1) {
-                            acertados.push(numeros[i]);
-                        } else {
-                            errados.push(numeros[i]);
-                        }
-                    };
-                    $('input[type="checkbox"]:checked').prop('checked', false);
-                    $("#numAcertados").text(acertados);
-                    $("#numErrados").text(errados);
-                    $("#numAcertadosTotal").text(acertados.length);
-                },
-                error: function(error) {
-                    console.log('Não entrou ' + error)
+                for (let i = 0; i < inputs.length; i++) {
+                    numeros[i] = inputs[i].value
                 }
-            })
+
+                var url = "https://apiloterias.com.br/app/resultado?loteria=" + concurso + "&token=" + token + "&concurso=" + numConcurso;
+
+                $.ajax({
+                    url,
+                    type: "get",
+                    dataType: "json",
+                    success: function(data) {
+                        var numResultado = data.dezenas
+
+                        for (var i = 0; i < numeros.length; i++) {
+                            if (numResultado.indexOf(numeros[i]) > -1) {
+                                acertados.push(numeros[i]);
+                            } else {
+                                errados.push(numeros[i]);
+                            }
+                        };
+                        $('input[type="checkbox"]:checked').prop('checked', false);
+                        $("#numAcertados").text(acertados);
+                        $("#numErrados").text(errados);
+                        $("#numAcertadosTotal").text(acertados.length);
+                    },
+                    error: function(error) {
+                        console.log('Não entrou ' + error)
+                    }
+                })
+            } else {
+                alert('Selecione algum número!')
+            }
         } else {
-            alert('Selecione algum número!')
+            alert('Selecione um quantidade de números valida!')
         }
     })
 })
 
 // 03 06 10 17 34 37
+
+let contador = ''
+
+var diadesorte = false
+var lotofacil = false
+var lotomania = false
+var megasena = false
+var quina = false
+
+$(document).on('change', 'input[type="checkbox"]:checked', function() {
+    contador = contador + 1
+    const filhos = document.getElementById('selConcurso')
+    let opcao = filhos.children[filhos.selectedIndex].value
+    if (opcao == 'diadesorte' && contador >= 7 && contador <= 15) {
+        diadesorte = true
+    } else {
+        diadesorte = false
+    }
+    if (opcao == 'lotofacil' && contador >= 15 && contador <= 20) {
+        lotofacil = true
+    } else {
+        lotofacil = false
+    }
+    if (opcao == 'lotomania' && contador == 50) {
+        lotomania = true
+    } else {
+        lotomania = false
+    }
+    if (opcao == 'megasena' && contador >= 6 && contador <= 15) {
+        megasena = true
+    } else {
+        megasena = false
+    }
+    if (opcao == 'quina' && contador >= 5 && contador <= 15) {
+        quina = true
+    } else {
+        quina = false
+    }
+})
--- JavaScript/Praticando/loteria/javascript/div_apostados.js
+++ JavaScript/Praticando/loteria/javascript/div_apostados.js
@@ -1,13 +1,16 @@
 let quantidade = ''
 var divAposta = document.getElementById('numAposta')
 
+
 $(document).ready(function() {
     var quantidade = '';
     $('#selConcurso').change(function() {
+        contador = 0
         quantidade = this.children[this.selectedIndex].textContent
         quantidade = parseInt(quantidade, 10);
         if (quantidade) {
             divAposta.innerHTML = ''
+
             for (let i = 1; i <= quantidade; i++) {
 
                 if (i < 10) {
@@ -17,19 +20,11 @@ $(document).ready(function() {
                     $('#numAposta').append(`<input type="checkbox" value="${i}"id="bolinha${i}">`)
                     $('head').append(`<style>#bolinha${i}:before {content: '${i}'}`)
                 }
-
             }
+
         } else {
+
             divAposta.innerHTML = 'Selecione um concurso!'
-        }
-    })
-    var inputs = document.querySelectorAll('input[type="checkbox"]:checked')
-    $('input[type="checkbox"]').click(function() {
-        alert('opa click')
-    })
-    $(document).find('input[type="checkbox"]').each(function() {
-        if ($(this).prop("checked") == "checked") {
-            alert('opa')
         }
     })
 
